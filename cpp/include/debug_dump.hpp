4bbcaac20beb7245ee46ba3202b4dd67b95ca0b7
Yuichi Motoyama <yomichi@imac2.todo-lab.t.u-tokyo.ac.jp>
2013-02-08 21:33:02 +0900

    fn to macro

--- cpp/include/debug_dump.hpp
+++ cpp/include/debug_dump.hpp
@@ -1,11 +1,11 @@
 #undef debug_dump
 #undef debug_stream
 
-#ifdef DEBUG_DUMP
-#define debug_dump(arg) std::cerr << arg << std::endl
-#define debug_dump(arg, cond) if(cond) std::cerr << arg << std::endl
-#define debug_stream(arg, os) os << arg << std::endl
-#define debug_stream(arg, os, cond) if(cond) os << arg << std::endl
+#ifdef NDEBUG
+#define debug_dump(arg) do{ std::cerr << arg << std::endl;}while(0)
+#define debug_dump(arg, cond) do{if(cond) std::cerr << arg << std::endl;}while(0)
+#define debug_stream(arg, os) do{os << arg << std::endl;}while(0)
+#define debug_stream(arg, os, cond) do{if(cond) os << arg << std::endl;}whjile(0)
 #else
 #define debug_dump(arg) ((void)0)
 #define debug_dump(arg, cond)  ((void)0)
