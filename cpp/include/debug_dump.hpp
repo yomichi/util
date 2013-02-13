016dcb3fe253704a062ed7a3454c641e934ab4a6
Yuichi Motoyama <yomichi@imac2.todo-lab.t.u-tokyo.ac.jp>
2013-02-13 17:15:52 +0900

    multi variable debug_dump

--- cpp/include/debug_dump.hpp
+++ cpp/include/debug_dump.hpp
@@ -1,15 +1,100 @@
-#undef debug_dump
-#undef debug_stream
+#ifndef DEBUG_DUMP_HPP
+#define DEBUG_DUMP_HPP
 
-#ifdef NDEBUG
-#define debug_dump(arg) do{ std::cerr << arg << std::endl;}while(0)
-#define debug_dump(arg, cond) do{if(cond) std::cerr << arg << std::endl;}while(0)
-#define debug_stream(arg, os) do{os << arg << std::endl;}while(0)
-#define debug_stream(arg, os, cond) do{if(cond) os << arg << std::endl;}whjile(0)
+#include <iostream>
+#include <boost/preprocessor/repetition.hpp>
+#include <boost/preprocessor/iteration/local.hpp>
+#include <boost/preprocessor/punctuation/comma_if.hpp>
+
+#define DD_ARG_MAX 25
+
+#define DD_arg(z,n,unused) BOOST_PP_COMMA_IF(n) T##n const& v##n
+#define DD_body(z,n,unused) << v##n
+
+#ifndef NDEBUG
+#define DD_dump(z,n,unused) \
+template <BOOST_PP_ENUM_PARAMS(n, class T)> \
+inline void debug_dump( BOOST_PP_REPEAT(n, DD_arg, ~) )\
+{ std::cerr \
+    BOOST_PP_REPEAT(n, DD_body, ~) \
+  << std::endl; }
 #else
-#define debug_dump(arg) ((void)0)
-#define debug_dump(arg, cond)  ((void)0)
-#define debug_stream(arg, os) ((void)0)
-#define debug_stream(arg, os, cond) ((void)0)
+#define DD_dump(z,n,unused) \
+template <BOOST_PP_ENUM_PARAMS(n, class T)> \
+inline void debug_dump( BOOST_PP_REPEAT(n, DD_arg, ~) ){ }
+#endif
+      
+#ifndef NDEBUG
+#define DD_dump_if(z,n,unused) \
+template <BOOST_PP_ENUM_PARAMS(n, class T)> \
+inline void debug_dump_if(bool cond, BOOST_PP_REPEAT(n, DD_arg, ~) )\
+{ if(cond)\
+  std::cerr \
+  BOOST_PP_REPEAT(n, DD_body, ~) \
+  << std::endl; }
+#else
+#define DD_dump_if(z,n,unused) \
+template <BOOST_PP_ENUM_PARAMS(n, class T)> \
+inline void debug_dump_if(bool cond, BOOST_PP_REPEAT(n, DD_arg, ~) ){ }
 #endif
 
+#ifndef NDEBUG
+#define DD_stream(z,n,unused) \
+template <class OSTREAM, BOOST_PP_ENUM_PARAMS(n, class T)> \
+inline void debug_stream(OSTREAM &os, BOOST_PP_REPEAT(n, DD_arg, ~) )\
+{ os \
+  BOOST_PP_REPEAT(n, DD_body, ~) \
+  << std::endl; }
+#else
+#define DD_stream(z,n,unused) \
+template <class OSTREAM, BOOST_PP_ENUM_PARAMS(n, class T)> \
+inline void debug_stream(OSTREAM &os, BOOST_PP_REPEAT(n, DD_arg, ~) ){ }
+#endif
+
+#ifndef NDEBUG
+#define DD_stream_if(z,n,unused) \
+template <class OSTREAM, BOOST_PP_ENUM_PARAMS(n, class T)> \
+inline void debug_stream_if(bool cond, OSTREAM &os, BOOST_PP_REPEAT(n, DD_arg, ~))\
+{ if(cond) os \
+  BOOST_PP_REPEAT(n, DD_body, ~) \
+  << std::endl; }
+#else
+#define DD_stream_if(z,n,unused) \
+template <class OSTREAM, BOOST_PP_ENUM_PARAMS(n, class T)> \
+inline void debug_stream_if(bool cond, OSTREAM &os, BOOST_PP_REPEAT(n, DD_arg, ~) ){ }
+#endif
+
+
+#define BOOST_PP_LOCAL_MACRO(n) DD_dump(~,n,~)
+#define BOOST_PP_LOCAL_LIMITS (1, DD_ARG_MAX+1)
+#include BOOST_PP_LOCAL_ITERATE()
+#undef BOOST_PP_LOCAL_LIMITS
+#undef BOOST_PP_LOCAL_MACRO
+
+#define BOOST_PP_LOCAL_MACRO(n) DD_dump_if(~,n,~)
+#define BOOST_PP_LOCAL_LIMITS (1, DD_ARG_MAX+1)
+#include BOOST_PP_LOCAL_ITERATE()
+#undef BOOST_PP_LOCAL_LIMITS
+#undef BOOST_PP_LOCAL_MACRO
+
+#define BOOST_PP_LOCAL_MACRO(n) DD_stream(~,n,~)
+#define BOOST_PP_LOCAL_LIMITS (1, DD_ARG_MAX+1)
+#include BOOST_PP_LOCAL_ITERATE()
+#undef BOOST_PP_LOCAL_LIMITS
+#undef BOOST_PP_LOCAL_MACRO
+
+#define BOOST_PP_LOCAL_MACRO(n) DD_stream_if(~,n,~)
+#define BOOST_PP_LOCAL_LIMITS (1, DD_ARG_MAX+1)
+#include BOOST_PP_LOCAL_ITERATE()
+#undef BOOST_PP_LOCAL_LIMITS
+#undef BOOST_PP_LOCAL_MACRO
+
+#undef DD_arg
+#undef DD_body
+#undef DD_dump
+#undef DD_dump_if
+#undef DD_stream
+#undef DD_stream_if
+#undef DD_ARG_MAX
+
+#endif // DEBUG_DUMP_HPP
---
+++ cpp/test/debug_dump.cpp
@@ -0,0 +1,10 @@
+#include "../include/debug_dump.hpp"
+
+int main()
+{
+  debug_dump("hoge", " ", 42);
+  debug_dump_if(true, "hoge", " ", 42);
+  debug_stream(std::cout, "hoge", " ", 137);
+  debug_stream_if(true, std::cout, "hoge", " ", 137);
+}
+
